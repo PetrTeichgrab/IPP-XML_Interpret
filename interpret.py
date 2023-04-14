61031d506ad6fa8326b9f81cd0b6a6802dc1ab1e
PetrTeichgrab <petr.teichgrab@seznam.cz>
2023-04-14 18:22:32 +0200

    implemented frame instructions and modified Frame class

--- interpret.py
+++ interpret.py
@@ -12,6 +12,8 @@ import argparse
 # • 57 - běhová chyba interpretace – špatná hodnota operandu (např. dělení nulou, špatná návra-
 # tová hodnota instrukce EXIT);
 # • 58 - běhová chyba interpretace – chybná práce s řetězcem.
+FrameInstructions = ["move", "createframe", "popframe", "defvar", "pushframe"]
+ArithmeticInstructions = ["add", "sub", "mul", "idiv"]
 
 exitCodes = {
     "SemanticCheckError" : 52,
@@ -51,10 +53,11 @@ class ArgsParser:
 
 class Instruction:
 
-    def __init__(self, order:int, opcode:str):
+    def __init__(self, order:int, opcode:str, frame):
         self._order = order
         self._opcode = opcode
         self._argList = []
+        self._frame = frame
     
     def AddArgument(self,arg):
         self._argList.append(arg)
@@ -70,35 +73,65 @@ class Instruction:
     
 
 class Frame:
-    frame = {}
     def __init__(self) -> None:
-        self.globalFrame = {}
-        self.localFrame = []
+        self.globalFrame = []
+        self.frameStack = []
         self.tempFrame = None
+        self._globalVarsList = []
+        self._localVarsList = []
+        self._tempVarsList = []
+        self.counter = 0
 
-    def Add(self,name, value):
-        self.frame[name] = value
+    def Add(self, variable):
+        if variable.GetFrame() == "GF":
+            self.globalFrame.append(variable)
+            self._globalVarsList.append(variable.GetName())
+        if variable.GetFrame() == "TF":
+            self.tempFrame.append(variable)
+            self._tempVarsList.append(variable.GetName())
+        if variable.GetFrame() == "LF":
+            self.frameStack[-1].append(variable)
+            self._localVarsList.append(variable.GetName())
     
-    def Remove(self, name):
-        del self.frame[name]
+    def Remove(self, variable):
+        if variable.GetFrame() == "GF":
+            self.globalFrame.remove(variable)
+            self._globalVarsList.remove(variable.GetName())
+        if variable.GetFrame() == "TF":
+            self.tempFrame.remove(variable)
+            self._tempVarsList.remove(variable.GetName())
+        if variable.GetFrame() == "LF":
+            self.frameStack[-1].remove(variable)
+            self._localVarsList.remove(variable.GetName())
+
+    def IsAlreadyDefined(self, variable):
+        
+        name = variable.GetName()
+        if variable.GetFrame() == "GF" and name in self._globalVarsList:
+            exit(exitCodes["SemanticCheckError"])
+        if variable.GetFrame() == "TF" and name in self._tempVarsList:
+            exit(exitCodes["SemanticCheckError"])
+        if variable.GetFrame() == "LF" and name in self._localVarsList:
+            exit(exitCodes["SemanticCheckError"])
 
     def CreateFrame(self):
-        self.tempFrame = {}
+        self.tempFrame = []
 
     def PushFrame(self):
-        self.localFrame.append(self.tempFrame)
+        self.frameStack.append(self.tempFrame)
+        self._localVarsList.extend(self._tempVarsList)
         self.tempFrame = None
     
     def PopFrame(self):
-        if bool(self.localFrame):
+        if bool(self._frameStackFrame):
             exit(exitCodes["NonExistingFrameError"])
-        self.tempFrame = self.localFrame[-1]
+        self.tempFrame = self._frameStack[-1]
 
 
 class ArithmeticInstruction(Instruction):
 
-        def __init__(self, order:int, opcode:str):
-            super().__init__(order, opcode)
+        def __init__(self, order:int, opcode:str, frame):
+            super().__init__(order, opcode, frame)
 
         def Add(var, sym1, sym2):
             var = sym1 + sym2
@@ -121,38 +154,50 @@ class ArithmeticInstruction(Instruction):
             return var
         
         def Execute(self):
-            if self._opcode.lower() == 'add' or 'sub' or 'mul' or 'idiv':
+            if self._opcode.lower() in ArithmeticInstructions:
                 TypeController.checkTypesThreeArgs(self._argList[0].GetArgType(), self._argList[1].GetArgType(), 
                                                self._argList[2].GetArgType(), "int", "int", "int")
                 #calling appropriate function
-                getattr(self, self._opcode.capitalize())                     
+                #eval(self._opcode.capitalize() + (self._argList[0], self._argList[1], self._argList[2]))                    
 
 
 class FrameInstruction(Instruction):
-        def __init__(self, order:int, opcode:str, frames):
-            super().__init__(order, opcode)
-            self._frames = frames
-            self._varName = ""
+        def __init__(self, order:int, opcode:str, frame):
+            super().__init__(order, opcode, frame)
+            self._frame = frame
 
-        def Defvar(self, var):
-            if re.match(r'^(GF|LF|TF)@[-$&%*!?_A-Za-z]+[-$&%*!?_A-Za-z0-9]*$', var):
-                self._frame, self._varName = var.split('@')
+        def Defvar(self):
+            arg = self._argList[0].GetArgValue()
+            varFrame, varName = arg.split('@')
+            var = Var(varName, varFrame)
+            self._frame.IsAlreadyDefined(var)
+            self._frame.Add(var)
         
-            
+        def Createframe(self):
+            self._frame.CreateFrame()
+
+        def Pushframe(self):
+            self._frame.PushFrame()
+
+        def Execute(self):
+            if self._opcode.lower() in FrameInstructions:
+                eval("self." + self._opcode.capitalize() + "()") 
+                
 
 
-  
 class InstructionFactory:
     
-    def __init__(self, order, opcode) -> None:
+    def __init__(self, order, opcode, frame) -> None:
         self._order = order
         self._opcode = opcode.lower()
+        self._frame = frame
+
 
     def GetInstructionType(self):
-        if self._opcode == "add" or "sub" or "mul" or "idiv" :
-            return ArithmeticInstruction(self._order, self._opcode)
-        if self._opcode == "move" or "createframe" or "popframe" or "defvar":
-            return FrameInstruction(self._order, self._opcode)
+        if self._opcode in FrameInstructions:
+            return FrameInstruction(self._order, self._opcode, self._frame)
+        if self._opcode in ArithmeticInstructions:
+            return ArithmeticInstruction(self._order, self._opcode, self._frame)
 
 
 class TypeController:
@@ -174,31 +219,77 @@ class InstructionArguments:
         return self._argValue
 
 
+class Var:
+    def __init__(self, name, frame) -> None:
+        self._name = name
+        self._frame = frame
+        self._type = None
+
+    def GetName(self):
+        return self._name
+    
+    def GetFrame(self):
+        return self._frame
+    
+    def GetType(self):
+        return self._type
+    
+    def SetType(self, type):
+        self._type = type
+
+    def SetFrame(self, frame):
+        self._frame = frame
+
+    def SetName(self, name):
+        self._name = name
+
+
+
+        
 class Interpret:
     def __init__(self) -> None:
-        pass
-        
+        self._args = ArgsParser()
+        self._tree = None
+        self._root = None
+        self._instructionList = []
 
-instructionList = []         
-args = ArgsParser()
-args.ProcessArgs()
-tree = args.GetXmlTree()
-root = tree.getroot()
-for ins in root:
-    instructionProps = list(ins.attrib.values())
-    instruction = InstructionFactory(int(instructionProps[0]), instructionProps[1]).GetInstructionType()
+    def makeXmlTree(self):
+        self._args.ProcessArgs()
+        self._tree = self._args.GetXmlTree()
+        self._root = self._tree.getroot()
 
-    for arg in ins:
-        instructionArg = InstructionArguments(list(arg.attrib.values()), arg.text)
-        instruction.AddArgument(instructionArg)
+    def ConvertXmlTreeToInstructList(self):
+        frame = Frame()
+        for ins in self._root:
+            instructionProps = list(ins.attrib.values())
+            instruction = InstructionFactory(int(instructionProps[0]), instructionProps[1], frame).GetInstructionType()
 
-    instructionList.append(instruction)
-        
-instructionList.sort(key=lambda x: x.GetOrder())
+            for arg in ins:
+                instructionArg = InstructionArguments(list(arg.attrib.values()), arg.text)
+                instruction.AddArgument(instructionArg)
+            
+            self._instructionList.append(instruction)
+
+        self._instructionList.sort(key=lambda x: x.GetOrder())
+
+    def GetInstructionList(self):
+        return self._instructionList
+    
+    def GetRoot(self):
+        return self._root     
+
+    def GetTree(self):
+        return self._tree   
+    
+    def start(self):
+        self.makeXmlTree()
+        self.ConvertXmlTreeToInstructList()
+        for instruction in self._instructionList:
+            instruction.Execute()
 
 
-instructionList[0].GetOrder()
-
+i = Interpret()
+i.start()
 
 
 
